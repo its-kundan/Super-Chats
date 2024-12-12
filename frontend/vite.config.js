5ac922005fd90837cfdf91c18897bb21cd8ad1c4
Kundan <kundan51kk@gmail.com>
2024-12-12 18:54:28 +0530

    error fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    // Set the chunk size warning limit to 2000 KiB (approximately 2 MiB)
+    chunkSizeWarningLimit: 2000,
+  },
   plugins: [react()],
 })
